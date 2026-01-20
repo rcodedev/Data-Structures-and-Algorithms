3440e5e5daa260536233ea5901dcb0315fbc6205
Rahul <rahul819723@gmail.com>
2026-01-20 23:05:47 +0530

    Recursion On Factorial

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		System.out.println(fact(5));
+	}
+	
+	public static int fact(int no) {
+		if(no==1)
+			return no;
+		else
+			return no*fact(no-1);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author appuy
+ *
+ */
+module Data_structures {
+}
